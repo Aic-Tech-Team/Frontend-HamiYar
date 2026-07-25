af48ebfd2e15c1811f728bf1d0d2deffe8af2d2b
hosseinNiknia <hossein.niknia@gmail.com>
2026-07-25 20:13:35 +0330

    fix: fixed error for landinghero

--- src/types/typewriter-effect-core.d.ts
+++ src/types/typewriter-effect-core.d.ts
@@ -1,6 +1,7 @@
-import type { TypewriterClass, Options } from "typewriter-effect";
-
 declare module "typewriter-effect/dist/core" {
+  import type { TypewriterClass, Options } from "typewriter-effect";
+
   const Typewriter: new (container: string | HTMLElement, options?: Options) => TypewriterClass;
+
   export default Typewriter;
 }
