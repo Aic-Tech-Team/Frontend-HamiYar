7418f5d633956a7ca9aeb7a6f4d350878eccb225
BenyaminKeshavarz <beny.keshavarz2018@gmail.com>
2026-07-25 20:26:17 +0330

    chore: add vite server port

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,16 @@ import Icons from "unplugin-icons/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
+
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+
   plugins: [vue(), vueDevTools(), tailwindcss(), Icons({ compiler: "vue3" })],
+
   define: { "process.env": {} },
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
