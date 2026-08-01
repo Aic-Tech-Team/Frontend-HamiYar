374c0a93b5ec6e56b1920d760b6633e79dd94170
hosseinNiknia <hossein.niknia@gmail.com>
2026-08-01 19:34:12 +0330

    fix: fixed the link to landing for header

--- src/components/dashboard/HeaderSideBar.vue
+++ src/components/dashboard/HeaderSideBar.vue
@@ -1,31 +1,24 @@
 <script setup lang="ts">
-
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-
+import { RouterLink } from "vue-router";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 </script>
 
 <template>
   <SidebarMenu>
     <SidebarMenuItem>
-      <DropdownMenu>
-        <DropdownMenuTrigger as-child>
-          <SidebarMenuButton
-            size="lg"
-            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+      <SidebarMenuButton size="lg" as-child class="hover:bg-transparent active:bg-transparent">
+        <RouterLink to="/">
+          <div
+            class="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground"
           >
-            <div
-              class="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground"
-            >
-              <img src="@/assets/images/logo.svg" />
-            </div>
-            <div class="grid flex-1 text-right text-sm leading-tight">
-              <span class="truncate font-medium"> سامانه حامیار </span>
-              <span class="truncate text-xs">سامانه یکپارچه</span>
-            </div>
-          </SidebarMenuButton>
-        </DropdownMenuTrigger>
-      </DropdownMenu>
+            <img src="@/assets/images/logo.svg" />
+          </div>
+          <div class="grid flex-1 text-right text-sm leading-tight">
+            <span class="truncate font-medium"> سامانه حامیار </span>
+            <span class="truncate text-xs">سامانه یکپارچه</span>
+          </div>
+        </RouterLink>
+      </SidebarMenuButton>
     </SidebarMenuItem>
   </SidebarMenu>
 </template>
